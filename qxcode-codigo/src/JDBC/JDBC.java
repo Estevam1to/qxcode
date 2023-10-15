d7740d50aef4bb6673e5c9929f1b7bca81acd9a6
Estevam1to <estevamchaves@alu.ufc.br>
2023-10-15 02:40:28 -0300

    criando cnx com o banco e controlador de tela

---
+++ qxcode-codigo/src/JDBC/JDBC.java
@@ -0,0 +1,23 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC {
+    private static Connection conn = null;
+    static {
+        String url = "jdbc:postgres://localhost:5432/postgres";
+        String user = "postgres";
+        String password = "1234";
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    public static Connection getConnection() {
+        return conn;
+    }
+}
--- qxcode-codigo/src/qxcode_implements/Controller/TelaQuestion.java
+++ qxcode-codigo/src/qxcode_implements/Controller/TelaQuestion.java
@@ -1,4 +1,43 @@
 package qxcode_implements.Controller;
 
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
 public class TelaQuestion {
+    @FXML
+    private ResourceBundle resources;
+    @FXML
+    private URL location;
+    @FXML
+    private TextField entradaUsuario;
+    @FXML
+    private Button btnSubmeter;
+
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void inicializa() {
+        location = getClass().getResource("../View/telaQuestion.fxml");
+        resources = ResourceBundle.getBundle("qxcode_implements.Controller.telaQuestion");
+    }
+
+    @FXML
+    public void entradaUsuario() {
+        String entrada = entradaUsuario.getText();
+        System.out.println(entrada);
+    }
+
+    @FXML
+    public void btnSubmeter() {
+        btnSubmeter.setOnAction(e -> {
+            entradaUsuario();
+            System.out.println("Submetido");
+        });
+    }
+
+    public String getTela() {
+        return "../View/telaQuestion.fxml";
+    }
 }
