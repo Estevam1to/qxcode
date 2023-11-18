0436ead69b1371ad6c45586b257b35c42ee9069d
nildo <elenildovas123@gmail.com>
2023-11-18 17:55:48 -0300

    Adicionando Controller para WriteInputOutput

---
+++ qxcode-codigo/src/main/java/com/qxcode/Controller/ControllerWriteInputOutputInFile.java
@@ -0,0 +1,19 @@
+package com.qxcode.Controller;
+import com.qxcode.Utils.WriteInputOutputInFile;
+
+public class ControllerWriteInputOutputInFile {
+
+    private WriteInputOutputInFile writeInputOutput = new WriteInputOutputInFile();
+
+    ControllerWriteInputOutputInFile () {
+        this.writeInputOutput = new WriteInputOutputInFile();
+    }
+
+    public void WriteInputByQuestionId(int id) {
+        writeInputOutput.WriteInputsByQuestionId(id);
+    }
+
+    public void WriteOutputByQuestionId(int id) {
+        writeInputOutput.WriteOutputsByQuestionId(id);
+    }
+}
