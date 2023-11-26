2f5ba2ba488c329e426855d7e38278f32eb10248
nildo <elenildovas123@gmail.com>
2023-11-26 15:59:23 -0300

    Addressing requested changes

--- qxcode-codigo/src/main/java/com/qxcode/DAO/QuestionDAO.java
+++ qxcode-codigo/src/main/java/com/qxcode/DAO/QuestionDAO.java
@@ -10,6 +10,20 @@ import com.qxcode.Model.Question;
 
 public class QuestionDAO {
 
+    public Question resultSetToQuestion(ResultSet resultSet) throws SQLException {
+
+        Question question = new Question(resultSet.getInt("id_questao"),
+        resultSet.getString("descricao"),
+        resultSet.getString("titulo"),
+        resultSet.getInt("dificuldade"),
+        resultSet.getString("exemplos"),
+        resultSet.getInt("id_categoria"),
+        resultSet.getInt("favorito")
+        );
+
+        return question;
+    }
+
     public List<Question> getQuestionsByCategory(int categoryId){
         List<Question> questions = new ArrayList<>();
         String sql = "SELECT * FROM questao WHERE id_categoria = ?";
@@ -22,17 +36,10 @@ public class QuestionDAO {
             ResultSet resultSet = preparedStatement.executeQuery();
 
             while (resultSet.next()) {
-                int id = resultSet.getInt("id_categoria");
-                String title = resultSet.getString("titulo");
-                String description = resultSet.getString("descricao");
-                int difficulty = resultSet.getInt("dificuldade");
-                String examples = resultSet.getString("exemplos");
-                int categId = resultSet.getInt("id_categoria");
-                int favorite = resultSet.getInt("favorito");
 
-
-                Question question = new Question(id, title, description, difficulty, examples, categId, favorite);
+                Question question = resultSetToQuestion(resultSet);
                 questions.add(question);
+
             }
 
             resultSet.close();
@@ -53,30 +60,23 @@ public class QuestionDAO {
              PreparedStatement preparedStatement = conn.prepareStatement(sql);
              ResultSet resultSet = preparedStatement.executeQuery();){
 
-
             while (resultSet.next()) {
-                int id = resultSet.getInt("id_categoria");
-                String title = resultSet.getString("titulo");
-                String description = resultSet.getString("descricao");
-                int difficulty = resultSet.getInt("dificuldade");
-                String examples = resultSet.getString("exemplos");
-                int categId = resultSet.getInt("id_categoria");
-                int favorite = resultSet.getInt("favorito");
 
-
-                Question question = new Question(id, title, description, difficulty, examples, categId, favorite);
+                Question question = resultSetToQuestion(resultSet);
                 questions.add(question);
+
             }
 
             resultSet.close();
             preparedStatement.close();
+
             return questions;
         } catch (SQLException e) {
             e.printStackTrace();
             return null;
         }
     }
-    public void insertQuestion(String title, String description, Integer difficulty, String examples, int categoryId) {
+    public void insertQuestion(String title, String description, int difficulty, String examples, int categoryId) {
         String sql = "INSERT INTO questao (titulo, descricao, dificuldade, exemplos, id_categoria) VALUES (?, ?, ?, ?, ?)";
 
         try (Connection conn = JDBC.getConnection()) {
@@ -125,20 +125,13 @@ public class QuestionDAO {
              ResultSet resultSet = preparedStatement.executeQuery();){
 
             while (resultSet.next()) {
-                int id = resultSet.getInt("id_questao");
-                String description = resultSet.getString("descricao");
-                String title = resultSet.getString("titulo");
-                int difficulty = resultSet.getInt("dificuldade");
-                String examples = resultSet.getString("exemplos");
-                int categoryId = resultSet.getInt("id_categoria");
-                int favorite = resultSet.getInt("favorito");
-
-                Question question = new Question(id, description, title, difficulty, examples, categoryId, favorite);
+                Question question = resultSetToQuestion(resultSet);
                 questions.add(question);
             }
 
             resultSet.close();
             preparedStatement.close();
+
             return questions;
         } catch (SQLException e) {
             e.printStackTrace();
@@ -148,24 +141,17 @@ public class QuestionDAO {
     }
 
     public Question getQuestionByTitle(String titulo) {
-        Question question = null;
         String sql = "SELECT * FROM questao WHERE titulo = ?";
         try (Connection conn = JDBC.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
             stmt.setString(1, titulo);
-            ResultSet rs = stmt.executeQuery();
+            ResultSet resultSet = stmt.executeQuery();
 
-                int id = rs.getInt("id_questao");
-                String title = rs.getString("titulo");
-                String description = rs.getString("descricao");
-                int difficulty = rs.getInt("dificuldade");
-                String examples = rs.getString("exemplos");
-                int categoryId = rs.getInt("id_categoria");
-                int favorite = rs.getInt("favorito");
+            Question question = resultSetToQuestion(resultSet);
 
-                question = new Question(id, description, title, difficulty, examples,categoryId, favorite);
-
+            resultSet.close();
+            stmt.close();
 
             return question;
         } catch (SQLException e) {
